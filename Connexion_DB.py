223e5b9603c1f77f73124fb35c676bd42c6e6d96
Anderson <maxxd814@gmail.com>
2023-11-06 19:34:27 -0500

    NO TOCAR

---
+++ Connexion_DB.py
@@ -0,0 +1,13 @@
+import os
+import sqlite3
+class Base_Datos:
+    def __init__(self):
+        self.Base_de_datos = "DB_Gestor_proyecyo.db"
+        self.directorio_actual = os.path.dirname(os.path.abspath(__file__))
+        self.ruta = os.path.join(self.directorio_actual, self.Base_de_datos)
+        if os.path.exists(self.ruta):
+            self.conexion = sqlite3.connect(self.ruta)
+            self.cursor = self.conexion.cursor()
+            print("Conectado a la base de datos .")
+        else:
+            print("La base de datos no se encontró")
