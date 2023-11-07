104ecf79f90d7fd13c5bf4e64b8e6769bf8bf197
Anderson <maxxd814@gmail.com>
2023-11-07 16:49:31 -0500

    avanze 4

--- gestor_2.py
+++ gestor_2.py
@@ -1,17 +1,21 @@
 from tkinter import *
+
 class Crud_General:
     def __init__(self):
         self.ventana = Tk()
         self.ventana.geometry("300x100+850+350")
         self.ventana.title("BackPack")
         self.ventana.minsize(200,100)
+        def alaraco():
+
 
         self.botton = Button()
         self.botton.config(text="Productos",font=("Ariel",10,"bold"))
         self.botton.place(x=100, y=10, width = 100, height = 30 )
 
         self.botton2 = Button()
-        self.botton2.config(text="Proveedores",font=("Ariel",10,"bold"))
+        self.botton2.config(text="Proveedores",font=("Ariel",10,"bold"),command=alaraco)
         self.botton2.place(x=100, y=50, width = 100, height = 30 )
 
         self.ventana.mainloop()
+Crud_General()
--- gestor_3.py
+++ gestor_3.py
@@ -1,77 +1,110 @@
 from tkinter import *
-class Crud_Provedor:
-    def __init__(self):
-        self.ventana = Tk()
-        self.ventana.geometry("230x150+850+300")
+from tkinter import messagebox as mb
+from Connexion_DB import *
+class Crud_Provedores:
+    def __init__(self, ventana):
+        self.ventana = ventana
+        self.ventana.geometry("230x200+850+300")
         self.ventana.title("BackPack")
         self.ventana.resizable(False, False)
-        # ventana.minsize(600,700)
-        self.texto = Label(text="CRUD PROVEDOR")
-        self.texto.config(font=("Ariel", 10, "bold"))
-        self.texto.pack()
+
+        self.bs = Base_Datos()
+
+        self.crear_interfaz()
 
 
-        def agregar():
-            pantallaN = Toplevel(self.ventana)
-            pantallaN.geometry("350x180+850+250")
-            pantallaN.grid_columnconfigure(0, weight=1)
-            pantallaN.grid_columnconfigure(1, weight=1)
+    def crear_interfaz(self):
+        texto = Label(text="CRUD PROVEEDOR")
+        texto.config(font=("Ariel", 10, "bold"))
+        texto.pack()
 
-            # ----------------------------------------------------------
+        # ----------------------------------------------------------
 
-            text_datos = Label(pantallaN, text="Ingrese los Datos del Proveedor")
-            text_datos.config(font=("Ariel", 10, "bold"))
-            text_datos.grid(row=0, column=0, columnspan=2, pady=5)
+        agregarBoton = Button(text="Agregar Proveedor", font=("Ariel", 8, "bold"), command=self.agregar)
+        agregarBoton.place(x=40, y=30, width=150, height=30)
 
-            # ----------------------------------------------------------
+        # ----------------------------------------------------------
 
-            textnombre = Label(pantallaN, text="IdProveedor: ")
-            textnombre.config(font=("Arial", 10, "bold"))
-            textnombre.grid(row=1, column=0, ipadx=5, pady=5)
-            textadd_nombre = Entry(pantallaN)
-            textadd_nombre.grid(row=1, column=1, ipadx=25, pady=5)
+        actuBoton = Button(text="Actualizar Proveedor", font=("Ariel", 8, "bold"), command=self.actualizar)
+        actuBoton.place(x=40, y=70, width=150, height=30)
 
-            # ----------------------------------------------------------
+        # ----------------------------------------------------------
 
-            textnombre = Label(pantallaN, text="Nombre: ")
-            textnombre.config(font=("Arial", 10, "bold"))
-            textnombre.grid(row=2, column=0, ipadx=5, pady=5)
-            textadd_nombre = Entry(pantallaN)
-            textadd_nombre.grid(row=2, column=1, ipadx=25, pady=5)
+        elimiBoton = Button(text="Eliminar Proveedor", font=("Ariel", 8, "bold"), command=self.eliminar)
+        elimiBoton.place(x=40, y=110, width=150, height=30)
 
-            # ----------------------------------------------------------
+    def agregar(self):
+        pantallaN = Toplevel(self.ventana)
+        pantallaN.geometry("350x215+850+250")
+        pantallaN.grid_columnconfigure(0, weight=1)
+        pantallaN.grid_columnconfigure(1, weight=1)
 
-            textcelu = Label(pantallaN, text="Telefono: ")
-            textcelu.config(font=("Arial", 10, "bold"))
-            textcelu.grid(row=3, column=0, ipadx=5, pady=5)
-            textadd_celu = Entry(pantallaN)
-            textadd_celu.grid(row=3, column=1, ipadx=25, pady=5)
+        # ----------------------------------------------------------
 
-            # ----------------------------------------------------------
+        text_datos = Label(pantallaN, text="Ingrese los Datos del Proveedor")
+        text_datos.config(font=("Ariel", 10, "bold"))
+        text_datos.grid(row=0, column=0, columnspan=2, pady=5)
 
-            textemp = Label(pantallaN, text="Empresa: ")
-            textemp.config(font=("Arial", 10, "bold"))
-            textemp.grid(row=4, column=0, ipadx=5, pady=5)
-            textadd_emp = Entry(pantallaN)
-            textadd_emp.grid(row=4, column=1, ipadx=25, pady=5)
-        def actualizar():
-            pantallaN = Toplevel(self.ventana)
-            pantallaN.geometry("300x400+850+250")
-        def eliminar():
-            pantallaN = Toplevel(self.ventana)
-            pantallaN.geometry("300x400+850+250")
+        # ----------------------------------------------------------
 
+        text_id = Label(pantallaN, text="IdProveedor: ")
+        text_id.config(font=("Arial", 10, "bold"))
+        text_id.grid(row=1, column=0, ipadx=5, pady=5)
+        self.textadd_id = Entry(pantallaN)
+        self.textadd_id.grid(row=1, column=1, ipadx=25, pady=5)
 
-        self.agregarBoton = Button()
-        self.agregarBoton.config(text="Agregar Provedor", font=("Ariel", 8, "bold"), command=agregar)
-        self.agregarBoton.place(x=40, y=30, width=150, height=30)
+        # ----------------------------------------------------------
 
-        self.actuBoton = Button()
-        self.actuBoton.config(text="Actualizar Provedor", font=("Ariel", 8, "bold"), command=actualizar)
-        self.actuBoton.place(x=40, y=70, width=150, height=30)
+        text_nombre = Label(pantallaN, text="Nombre: ")
+        text_nombre.config(font=("Arial", 10, "bold"))
+        text_nombre.grid(row=2, column=0, ipadx=5, pady=5)
+        self.textadd_nombre = Entry(pantallaN)
+        self.textadd_nombre.grid(row=2, column=1, ipadx=25, pady=5)
 
-        self.elimiBoton = Button()
-        self.elimiBoton.config(text="Eliminar Proveedor ", font=("Ariel", 8, "bold"), command=eliminar)
-        self.elimiBoton.place(x=40, y=110, width=150, height=30)
-        self.ventana.mainloop()
-Crud_Provedor()
+        # ----------------------------------------------------------
+
+        text_telefono = Label(pantallaN, text="Telefono: ")
+        text_telefono.config(font=("Arial", 10, "bold"))
+        text_telefono.grid(row=3, column=0, ipadx=5, pady=5)
+        self.textadd_telefono = Entry(pantallaN)
+        self.textadd_telefono.grid(row=3, column=1, ipadx=25, pady=5)
+
+        # ----------------------------------------------------------
+
+        text_empresa = Label(pantallaN, text="Empresa: ")
+        text_empresa.config(font=("Arial", 10, "bold"))
+        text_empresa.grid(row=4, column=0, ipadx=5, pady=5)
+        self.textadd_empresa = Entry(pantallaN)
+        self.textadd_empresa.grid(row=4, column=1, ipadx=25, pady=5)
+
+        # ----------------------------------------------------------
+
+        agregar_btn = Button(pantallaN, text="Agregar", font=("Arial", 10, "bold"), command=self.agregar_proveedor)
+        agregar_btn.grid(row=5, column=0, columnspan=2, pady=10)
+
+    def agregar_proveedor(self):
+        id = self.textadd_id.get()
+        nombre = self.textadd_nombre.get()
+        telefono = self.textadd_telefono.get()
+        empresa = self.textadd_empresa.get()
+
+        try:
+            self.bs.cursor.execute(
+                "INSERT INTO Proveedores (idProveedor, nombre, numero_telefono, empresa) VALUES (?, ?, ?, ?)",
+                (id, nombre, telefono, empresa))
+            self.bs.conexion.commit()
+            mb.showinfo("Éxito", "Proveedor agregado con éxito.")
+        except sqlite3.Error as e:
+            mb.showerror("Error", "Error al agregar el proveedor: " + str(e))
+
+    def actualizar(self):
+        pantallaN = Toplevel(self.ventana)
+        pantallaN.geometry("300x400+850+250")
+
+    def eliminar(self):
+        pantallaN = Toplevel(self.ventana)
+        pantallaN.geometry("300x400+850+250")
+
+ventana = Tk()
+app = Crud_Provedores(ventana)
+ventana.mainloop()
