45b4ad4dadaf0ceba8d51b991d2fecc2a9de32dd
Anderson <maxxd814@gmail.com>
2023-11-10 00:41:31 -0500

    E SALVADO MI PROGRESO CSRM

--- Login.py
+++ Login.py
@@ -4,21 +4,24 @@ class inicio_seccion:
     def login(tex_input1,tex_input2):
         bs = Base_Datos()
         try:
-            id = tex_input1
-            contra = tex_input2
+            id = tex_input1.get()
+            contra = tex_input2.get()
             bs.cursor.execute("SELECT * FROM usuarios WHERE Id = ? AND contrasenna = ? ", (id, contra))
-            bs.cursor.fetchall()
+            return bs.cursor.fetchall()
         except sqlite3.Error as e:
             print("error al iniciar secion {}".format(e))
 
     @staticmethod
     def agregar_registro(ID_registro,CONTRA_registro):
-        bs = Base_Datos()
-        id = ID_registro.get()
-        contra =CONTRA_registro.get()
-        bs.cursor.execute(
-            "INSERT INTO usuarios (Id, contrasenna) VALUES (?, ?)",
-            (id,contra))
-        bs.conexion.commit()
-
+        try:
+            bs = Base_Datos()
+            id = ID_registro.get()
+            contra =CONTRA_registro.get()
+            bs.cursor.execute(
+                "INSERT INTO usuarios (Id, contrasenna) VALUES (?, ?)",
+                (id,contra))
+            bs.conexion.commit()
+            return True
+        except sqlite3.Error as e:
+            print("error al iniciar secion {}".format(e))
 
