1da25cfa1f411d1d504c76919647cbd8ca3d43d6
Deyvi <deyvioscarzanches@gmail.com>
2023-11-27 13:41:23 -0500

    hash

--- CONSULTA_LOGIN.py
+++ CONSULTA_LOGIN.py
@@ -1,16 +1,23 @@
 from Connexion_DB import *
 import sqlite3
+from passlib.context import CryptContext
 class Login:
     def __init__(self):
         self.db = Base_Datos().conexion
         self.miCursor = self.db.cursor()
+        self.contexto = CryptContext(schemes=["pbkdf2_sha256"], default="pbkdf2_sha256")
     def verificacion(self,nombreUsuario,claveUsuario):
         try:
-            self.miCursor.execute("SELECT * FROM Usuarios WHERE Nombre=? AND Clave=?",
-                                  (nombreUsuario, claveUsuario))
+            self.miCursor.execute("SELECT * FROM Usuarios WHERE Nombre=?", (nombreUsuario,))
             datos = self.miCursor.fetchall()
-            self.db.close()
-            return datos
+            for row in datos:
+                Hash = row[3]
+
+                # Verificar la contraseña ingresada con el hash almacenado
+            if self.contexto.verify(claveUsuario, Hash):
+                return datos
+            else:
+                return []
 
         except sqlite3.Error as e:
             print(f"Error en mostrar proveedor: {e}")
